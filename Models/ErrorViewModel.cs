16be848358773201edf1990500c741338aaa1b6a
ben <{username}@users.noreply.github.com>
2024-01-04 15:18:20 -0600

    init asp.net core mvc app and generate .gitignore thru dotnet cli

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace plumbus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
